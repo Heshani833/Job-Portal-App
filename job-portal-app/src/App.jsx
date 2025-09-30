ce2eddb04e71c2b6aaffbfc86dd6f2dc08f7d716
Heshani833 <heshanishehana123@gmail.com>
2025-09-30 22:47:14 +0530

    Setting multiple routes

--- job-portal-app/src/App.jsx
+++ job-portal-app/src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import ApplyJob from "./Pages/ApplyJob";
+import Application from "./Pages/Application";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/application" element={<Application />} />
+        <Route path="/apply-job" element={<ApplyJob />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
