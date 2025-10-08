47c068aa47d6a2e9df0c1b0afaab62d01c547bdc
Heshani833 <heshanishehana123@gmail.com>
2025-10-08 22:10:40 +0530

    Added icons

--- job-portal-app/src/Components/JobListing.jsx
+++ job-portal-app/src/Components/JobListing.jsx
@@ -1,22 +1,35 @@
 import React from "react";
 import { useContext } from "react";
 import { AppContext } from "../Context/AppContext";
+import { assets } from "../assets/assets";
 
 const JobListing = () => {
   const { searchFilter, isSearched } = useContext(AppContext);
 
   return (
     <div>
-      {/* side bar */}
       <div>
-        {/* search filter from hero components */}
         {isSearched && (searchFilter.title || searchFilter.location) ? (
           <div className="p-4 bg-gray-100 rounded-lg m-4">
             <h3 className="text-lg font-medium mb-2">Search Filters:</h3>
-            <p>{searchFilter.title && `Title: ${searchFilter.title}`}</p>
-            <p>
-              {searchFilter.location && `Location: ${searchFilter.location}`}
-            </p>
+            <span>
+              <p>{searchFilter.title && `Title: ${searchFilter.title}`}</p>
+              <img
+                className="cursor-pointer"
+                src={assets.cross_icon}
+                alt="cross-icon"
+              />
+            </span>
+            <span>
+              <p>
+                {searchFilter.location && `Location: ${searchFilter.location}`}
+                <img
+                  className="cursor-pointer"
+                  src={assets.cross_icon}
+                  alt="cross-icon"
+                />
+              </p>
+            </span>
           </div>
         ) : (
           <p className="p-4">No search filters applied.</p>
